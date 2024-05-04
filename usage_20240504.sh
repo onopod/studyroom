99890aa8bbb11d8f67885aa683a029d329373205
ono-ryo <onopod@gmail.com>
2024-05-04 14:08:02 +0900

    usageに本番サーバのパスを追加

--- usage_20240504.sh
+++ usage_20240504.sh
@@ -1,4 +1,5 @@
 d=http://localhost:8000/api/
+#d=https://onopod-1.paiza-user-basic.cloud:8000/api/
 echo -e "\nnightbotから飛んでくるcommandをテスト"
 curl --get ${d}users/ONOPOD/changestatus/ --data-urlencode id=ONOPOD --data-urlencode name=小野 --data-urlencode emote=smile
 curl --get ${d}users/ONOPOD/changestatus/ --data-urlencode id=ONOPOD --data-urlencode name=小野 --data-urlencode subject=簿記2級
