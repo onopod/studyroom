87327e26da0f9d6b97e66ef6190e487148b6e0d9
ono-ryo <onopod@gmail.com>
2024-05-10 18:46:52 +0900

    メモの追加

--- usage_spawn.sh
+++ usage_spawn.sh
@@ -2,10 +2,14 @@ d=http://localhost:8000/api/
 #d=https://onopod-1.paiza-user-basic.cloud:8000/api/
 echo -e "\nin out Spawnコマンドをテスト"
 curl --get ${d}studies/in/ --data-urlencode id=ONO123 --data-urlencode name=ONO
-curl --get ${d}studies/charge/ --data-urlencode id=ONO123 --data-urlencode name=ONO
-curl --get ${d}studies/out/ --data-urlencode id=ONO123 --data-urlencode name=ONO
 curl --get ${d}studies/in/ --data-urlencode id=SATO123 --data-urlencode name=SATO
-curl --get ${d}users/SATO123/changestatus/ --data-urlencode id=SATO123 --data-urlencode name=SATO --data-urlencode place=Village
+curl --get ${d}studies/in/ --data-urlencode id=TANAKA123 --data-urlencode name=TANAKA
+curl --get ${d}users/ONO123/changestatus/ --data-urlencode id=ONO123 --data-urlencode name=ONO --data-urlencode place=A
+curl --get ${d}users/SATO123/changestatus/ --data-urlencode id=SATO123 --data-urlencode name=SATO --data-urlencode place=A
+curl --get ${d}users/TANAKA123/changestatus/ --data-urlencode id=TANAKA123 --data-urlencode name=TANAKA --data-urlencode place=A
+curl --get ${d}users/ONO123/changestatus/ --data-urlencode id=ONO123 --data-urlencode name=ONO --data-urlencode place=B
+curl --get ${d}users/SATO123/changestatus/ --data-urlencode id=SATO123 --data-urlencode name=SATO --data-urlencode place=B
+curl --get ${d}users/TANAKA123/changestatus/ --data-urlencode id=TANAKA123 --data-urlencode name=TANAKA --data-urlencode place=B
 
 
 
