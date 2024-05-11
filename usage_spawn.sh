0d649e323cf2536a87ecc0587ba9d493283b14fe
ono-ryo <onopod@gmail.com>
2024-05-11 13:02:45 +0900

    usageをキャンバス確認用に変更

--- usage_spawn.sh
+++ usage_spawn.sh
@@ -4,12 +4,12 @@ echo -e "\nin out Spawnコマンドをテスト"
 curl --get ${d}studies/in/ --data-urlencode id=ONO123 --data-urlencode name=ONO
 curl --get ${d}studies/in/ --data-urlencode id=SATO123 --data-urlencode name=SATO
 curl --get ${d}studies/in/ --data-urlencode id=TANAKA123 --data-urlencode name=TANAKA
-curl --get ${d}users/ONO123/changestatus/ --data-urlencode id=ONO123 --data-urlencode name=ONO --data-urlencode place=A
-curl --get ${d}users/SATO123/changestatus/ --data-urlencode id=SATO123 --data-urlencode name=SATO --data-urlencode place=A
-curl --get ${d}users/TANAKA123/changestatus/ --data-urlencode id=TANAKA123 --data-urlencode name=TANAKA --data-urlencode place=A
-curl --get ${d}users/ONO123/changestatus/ --data-urlencode id=ONO123 --data-urlencode name=ONO --data-urlencode place=B
-curl --get ${d}users/SATO123/changestatus/ --data-urlencode id=SATO123 --data-urlencode name=SATO --data-urlencode place=B
-curl --get ${d}users/TANAKA123/changestatus/ --data-urlencode id=TANAKA123 --data-urlencode name=TANAKA --data-urlencode place=B
+#curl --get ${d}users/ONO123/changestatus/ --data-urlencode id=ONO123 --data-urlencode name=ONO --data-urlencode place=A
+#curl --get ${d}users/SATO123/changestatus/ --data-urlencode id=SATO123 --data-urlencode name=SATO --data-urlencode place=A
+#curl --get ${d}users/TANAKA123/changestatus/ --data-urlencode id=TANAKA123 --data-urlencode name=TANAKA --data-urlencode place=A
+#curl --get ${d}users/ONO123/changestatus/ --data-urlencode id=ONO123 --data-urlencode name=ONO --data-urlencode place=B
+#curl --get ${d}users/SATO123/changestatus/ --data-urlencode id=SATO123 --data-urlencode name=SATO --data-urlencode place=B
+#curl --get ${d}users/TANAKA123/changestatus/ --data-urlencode id=TANAKA123 --data-urlencode name=TANAKA --data-urlencode place=B
 
 
 
